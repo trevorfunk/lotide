0f30ba62f78b1e82435fae3a663aa3c23ddb0a79
trevorfunk <trevorjonfunk@gmail.com>
2022-05-18 17:12:52 -0700

    first commit for head.js

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+ if (actual !== expected) {
+   console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+ } else {
+   console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+ }
+};
+
+const head = (array) => {
+ let head = array.shift();
+ return head;
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([5,6,7]), 7);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
