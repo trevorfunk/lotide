eb45b111a8858b652f469c023d6aefb9d0526bc1
trevorfunk <trevorjonfunk@gmail.com>
2022-05-21 17:14:25 -0700

    first commit flatten.js

---
+++ flatten.js
@@ -0,0 +1,18 @@
+const flatten = (array) => {
+  newArr = [];
+  for (const element of array) {
+    if (!Array.isArray(element)) {
+      newArr.push(element);
+    }
+    if (Array.isArray(element)) {
+      for (const nestedElement of element) {
+        newArr.push(nestedElement);
+      }
+    }
+  }
+  return newArr;
+};
+
+console.log(flatten([[1], 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+flatten([[1], 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
+flatten([[1], 2, [3, 5], 5, 6, [11, 12]]); // => [1, 2, 3, 4, 5, 6]
