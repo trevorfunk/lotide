7bb9c912e6ce4fd9db7ebcf3a51ffbf91b44f6b5
trevorfunk <trevorjonfunk@gmail.com>
2022-05-31 12:43:35 -0700

    add assertArraysEqual and format document

--- takeUntil.js
+++ takeUntil.js
@@ -1,33 +1,33 @@
 const eqArrays = (array1, array2) => {
- if (array1.length !== array2.length) {
-   return false;
- }
- for (let i = 0; i < array1.length; i++) {
-   if (array1[i] !== array2[i]) {
-     return false;
-   }
- }
- return true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const assertArraysEqual = (array1, array2) => {
- if (!eqArrays(array1, array2)) {
-  return console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+  if (!eqArrays(array1, array2)) {
+    return console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
   }
   console.log(`🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
-}
+};
 
 
 const takeUntil = function(array, callback) {
- newArr = [];
-for (element of array) {
- if (callback(element) === false) {
-  newArr.push(element);
- } else {
-  break;
- }
-}
-return newArr;
+  const newArr = [];
+  for (const element of array) {
+    if (callback(element) === false) {
+      newArr.push(element);
+    } else {
+      break;
+    }
+  }
+  return newArr;
 };
 
 
@@ -42,3 +42,5 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
