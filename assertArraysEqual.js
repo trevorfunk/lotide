ee1facb42d4de558835dc7893cb5d3faebef132e
trevorfunk <trevorjonfunk@gmail.com>
2022-06-07 14:48:06 -0700

    refactor assertArraysEqual. Export asserArraysEqual and test.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = (array1, array2) => {
- if (array1.length !== array2.length) {
-   return false;
- }
- for (let i = 0; i < array1.length; i++) {
-   if (array1[i] !== array2[i]) {
-     return false;
-   }
- }
- return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (array1, array2) => {
  if (!eqArrays(array1, array2)) {
@@ -17,9 +7,4 @@ const assertArraysEqual = (array1, array2) => {
   console.log(`🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
 }
 
-
- assertArraysEqual([1, 2, 3], [1, 2, 3]) //true
- assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) //true
- assertArraysEqual([1, 2, 3], [1, 2, 3, 4]) //false
- assertArraysEqual([1, 2, 3], [3, 2, 1]) //false
- assertArraysEqual([1, 2, 3], [1, 2, "3"]) //false
+module.exports = assertArraysEqual;
